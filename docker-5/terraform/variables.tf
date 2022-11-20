2534b69ec38f2d0e045404e1b218c165479fbb27
mishalipatnikov <misha-lipatnikov@mail.ru>
2022-11-20 17:41:02 +0300

    docker 5

---
+++ docker-5/terraform/variables.tf
@@ -0,0 +1,20 @@
+variable "folder_id" {
+  type = string
+  default = "b1gvvtkibf0p4g3i3pgg"
+}
+
+variable "zone" {
+  type = string
+  default = "ru-central1-a"
+}
+
+variable "subnet" {
+  type = string
+  default = "e9be6jttamgv2fhmqu9k"
+}
+
+
+variable "cloud_id" {
+  type = string
+  default = "b1gvvtkibf0p4g3i3pgg"
+}
